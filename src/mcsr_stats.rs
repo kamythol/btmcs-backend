e2696dc7a3737e6513fd9410b4a014523bc93e8b
kamythol <reaml@tuta.io>
2026-01-10 17:21:29 -0800

    mcsr: update offsets

--- src/mcsr_stats.rs
+++ src/mcsr_stats.rs
@@ -81,7 +81,7 @@ async fn get_history() -> Result<Vec<match_history::GameData>, Error> {
     // let req = format!("https://mcsrranked.com/api/users/beasttrollmc/matches?count=100&type=2&after=4526605"); // 100
     // let req = format!("https://mcsrranked.com/api/users/beasttrollmc/matches?count=100&type=2&after=4424617"); // 160
 
-    let req = format!("https://mcsrranked.com/api/users/beasttrollmc/matches?count=100&type=2&after=4732008"); // 57
+    let req = format!("https://mcsrranked.com/api/users/beasttrollmc/matches?count=100&type=2&after=4857369"); // 139
     let client = reqwest::Client::new();
     let data = client.get(req).send().await?.json::<match_history::Response>().await?;
     Ok(data.data)
@@ -113,10 +113,10 @@ pub async fn get_counts() -> Counts {
     // let mut deaths: u32 = 135; // death count offset - last: 80
 
     // offsets break todays, only update at 0 utc
-    let mut matches: u32 = 57; // offset
-    let mut deaths: u32 = 34; // offset
-    let mut ffs_season: u32 = 2; // offset
-    let mut ff_wins_season: u32 = 5; // offset
+    let mut matches: u32 = 139; // offset
+    let mut deaths: u32 = 87; // offset
+    let mut ffs_season: u32 = 4; // offset
+    let mut ff_wins_season: u32 = 12; // offset
     let mut slowest_season: u32 = 0;
     
     let mut matches_today: u32 = 0;
